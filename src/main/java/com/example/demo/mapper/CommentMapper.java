c98cf1ba716afd94b865570ddf0053bc36f1bc37
donguk <scyqn84850933@gmail.com>
2023-05-22 23:51:27 +0900

    comment

---
+++ src/main/java/com/example/demo/mapper/CommentMapper.java
@@ -0,0 +1,63 @@
+package com.example.demo.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.domain.*;
+
+@Mapper
+public interface CommentMapper {
+
+	
+
+	@Select("""
+			SELECT * 
+			FROM Comment
+			WHERE boardId = #{boardId}
+			ORDER BY id
+			""")
+	List<Comment> selectAllByBoardId(Integer boardId);
+
+	@Insert("""
+			INSERT INTO Comment ( boardId, content, memberId)
+			VALUES (#{boardId}, #{content}, #{memberId})
+			""")
+	int insert(Comment comment);
+
+	@Delete("""
+			DELETE FROM Comment
+			WHERE id = #{id}
+			""")
+	Integer deleteById(Integer id);
+
+	@Select("""
+			SELECT * 
+			FROM Comment
+			WHERE id = #{id}
+			""")
+	Comment selectById(Integer id);
+
+	@Update("""
+			UPDATE Comment 
+			SET
+				content = #{content}
+			WHERE
+				id = #{id}
+			""")
+	int update(Comment comment);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
